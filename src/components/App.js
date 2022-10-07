2751f021008fe343aeaac479178184edc00ab4be
bvaibhav23 <108619671+bvaibhav23@users.noreply.github.com>
2022-10-07 23:30:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-  
   return (
     <div id="main">
-      <hi id="marco-polo">Marco</h1>;
-      <button id="marco-polo-toggler">Polo</button>
+      <h1 id="marco-polo">Marco</h1>
+      <button id="marco-polo-toggler" onClick={tog}>
+        Polo
+      </button>
     </div>
-  )
-}
+  );
+};
 
+const tog = () => {
+  let name = document.getElementById("marco-polo").innerText;
+  name === "Marco" ? (name = "Polo") : (name = "Marco");
+ // alert(name);
+  let btn = document.getElementById("marco-polo-toggler").innerText;
+  btn === "Marco" ? (btn = "Polo") : (btn = "Marco");
+//   alert(btn);
+};
 
 export default App;
